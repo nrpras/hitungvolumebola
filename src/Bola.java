2979a8560498926afba357a4a905e5caddc587f0
prasetyo nusantara <nrprasetyon@gmail.com>
2023-10-30 08:23:44 +0700

    first commit

--- src/Bola.java
+++ src/Bola.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Bola {
+public class Bola {
+    private double radius;
+
+    public Bola(double radius) {
+    }
+
+    public void bola(double radius){
+        this.radius = radius;
+    }
+    public double hitungvolumebola(){
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    public static void main(String[] args){
+        double radius = 5.0;
+        Bola bola = new Bola(radius);
+        double voulume = bola.hitungvolumebola();
+        System.out.println("volume bola dengan radius" + radius + "adalah: " + voulume);
+    }
 }
